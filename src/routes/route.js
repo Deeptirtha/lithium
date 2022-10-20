365989e593d5c6361c79fefe501dede9cf394622
Deeptirtha <tirtha1111222@gmail.com>
2022-10-20 16:07:03 +0530

    oct20 class work

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,5 +1,41 @@
 const express = require('express');
 const router = express.Router();
 
-
+let persons= [
+    {
+    name: "PK",
+    age: 10,
+    votingStatus: false
+ },
+ {
+    name: "SK",
+    age: 20,
+    votingStatus: false
+ },
+ {
+    name: "AA",
+    age: 70,
+    votingStatus: false
+ },
+ {
+    name: "SC",
+    age: 5,
+    votingStatus: false
+ },
+ {
+    name: "HO",
+    age: 40,
+    votingStatus: false
+ }
+ ]
+ 
+router.get("/voatingage",function(req,res){
+    let age=req.query.age
+    let final=persons.filter(a=>a.age>age)
+    for(i=0;i<final.length;i++){
+        final[i].votingStatus="true"
+    }
+    console.log(persons)
+    res.send("voater list is updated ")
+})
 module.exports = router;
