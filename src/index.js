e84d537f17b2eb394e2bc115cc0680f7d4ce1fd3
Deeptirtha <tirtha1111222@gmail.com>
2022-11-01 17:07:26 +0530

    middleware

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://DeeptirthaMukherjee:QYKI3k8QSKC4I7FZ@cluster1.khatgm1.mongodb.net/Deep-db?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
@@ -17,6 +17,7 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 app.use (
     function (req, res, next) {
         console.log ("inside GLOBAL MW");
+        console.log (req.ip);
         next();
   }
   );
