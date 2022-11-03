db880cd0ef964a0167d3edfba0f8088f8f65bb0e
Deeptirtha <tirtha1111222@gmail.com>
2022-11-03 20:09:05 +0530

    using midware

---
+++ src/middleware/commonmiddleware.js
@@ -0,0 +1,13 @@
+const jwt = require("jsonwebtoken");
+const validationMiddleware = function(req, res, next){
+
+    let token = req.headers["x-auth-token"];
+    console.log(token)
+  
+    if (!token) return res.send({ status: false, msg: "token must be present" });
+  
+    let decodedToken = jwt.verify(token, "functionup-lithium-secret-key");
+    if (!decodedToken) return res.send({ status: false, msg: "token is invalid" });
+    else{next()}
+}
+module.exports.validationMiddleware = validationMiddleware
