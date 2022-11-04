3face916d6938e476d12c07d5a5de5d25c977211
Deeptirtha <tirtha1111222@gmail.com>
2022-11-04 18:41:33 +0530

    okk

--- src/middleware/commonmiddleware.js
+++ src/middleware/commonmiddleware.js
@@ -7,6 +7,22 @@ const validationMiddleware = function(req, res, next){
   
     let decodedToken = jwt.verify(token, "functionup-lithium-secret-key");
     if (!decodedToken) return res.send({ status: false, msg: "token is invalid" });
-    else{next()}
+    else{
+        res.setHeader("x-auth-token",token)
+        next()}
+}
+
+const authorization = function(req, res, next){
+
+    let token = req.headers["x-auth-token"];
+   
+    if (!token) return res.send({ status: false, msg: "token must be present" });
+  
+    let decodedToken = jwt.verify(token, "functionup-lithium-secret-key");
+    if (!decodedToken) return res.send({ status: false, msg: "token is invalid" });
+    else{
+        res.setHeader("x-auth-token",token)
+        next()}
 }
 module.exports.validationMiddleware = validationMiddleware
+module.exports.authorization = authorization
