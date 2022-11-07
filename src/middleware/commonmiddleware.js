9498dadeaffec2e5653525fbccba156b8f895dae
Deeptirtha <tirtha1111222@gmail.com>
2022-11-07 21:03:27 +0530

    try&catch

--- src/middleware/commonmiddleware.js
+++ src/middleware/commonmiddleware.js
@@ -3,12 +3,12 @@ const validationMiddleware = function (req, res, next) {
     try {
         let token = req.headers["x-auth-token"];
 
-        if (!token) return res.send({ status: false, msg: "token must be present" });
+        if (!token) return res.status(400).send({ status: false, msg: "token must be present" });
 
         let decodedToken = jwt.verify(token, "functionup-lithium-secret-key");
 
         req.decodedToken = decodedToken
-        if (!decodedToken) return res.send({ status: false, msg: "token is invalid" });
+        if (!decodedToken) return res.status(400).send({ status: false, msg: "token is invalid" });
         else {
 
             next()
@@ -21,7 +21,8 @@ const validationMiddleware = function (req, res, next) {
 
 const authorization = function (req, res, next) {
     try {
-    if (req.decodedToken.userId !== req.params.userId) return res.send({ status: false, msg: "you do not have authorization to this " });
+       
+    if (req.decodedToken.userId !== req.params.userId) return res.status(400).send({ status: false, msg: "you do not have authorization to this " });
     else {
         next()
     }}
@@ -29,5 +30,7 @@ const authorization = function (req, res, next) {
         res.status(500).send({msg:err.message})
     }
 }
+
 module.exports.validationMiddleware = validationMiddleware
 module.exports.authorization = authorization
+
