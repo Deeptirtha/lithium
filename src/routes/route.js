b0424c469470c76aeaf5eec588217c8a92b90ae0
Deeptirtha <tirtha1111222@gmail.com>
2022-11-08 23:08:55 +0530

    done

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const weatherController= require("../controllers/weatherController")
+const weatherController= require("../controllers/weathercontroller")
 
 
 
