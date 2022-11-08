d92215db86ca48c3bc2b02148b1c0e2b58428c07
Deeptirtha <tirtha1111222@gmail.com>
2022-11-08 23:03:20 +0530

    weather

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const CowinController= require("../controllers/cowinController")
+const weatherController= require("../controllers/weatherController")
 
 
 
@@ -9,14 +9,9 @@ router.get("/test-me", function (req, res) {
 })
 
 
-router.get("/cowin/states", CowinController.getStates)
-router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
-router.get("/cowin/getByPin", CowinController.getByPin)
-router.get("/cowin/getBydistrict", CowinController.getByDistrict)
+router.get("/london", weatherController.weatherofLondon)
+router.get("/allcity", weatherController.weatherofallcity)
 
-router.post("/cowin/getOtp", CowinController.getOtp)
-
-// WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
 
 
 
